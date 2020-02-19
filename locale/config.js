3405b32923f5fbc071874a3c19ebe8eee1a986f6
jhlee838 <jhlee838@gmail.com>
2020-02-19 17:53:53 +0900

    tr

--- locale/config.js
+++ locale/config.js
@@ -1,14 +1,14 @@
 moment.locale('ko', {
-    months: '1월,2월_3월_4월_5월_6월_7월_8월_9월_10월_11월_12월'.split('_'),
-    monthsShort: '1 월_2월_3 월_4 월_5 월_6 월_7 월_8 월_9 월_10 월_11 월_12 월'.split('_'),
+    months: '1월_2월_3월_4월_5월_6월_7월_8월_9월_10월_11월_12월'.split('_'),
+    monthsShort: '1월_2월_3월_4월_5월_6월_7월_8월_9월_10월_11월_12월'.split('_'),
     weekdays: '일요일_월요일_화요일_수요일_목요일_금요일_토요일'.split('_'),
     weekdaysShort: '일_월_화_수_목_금_토'.split('_'),
     weekdaysMin : '일._월._화._수._목._금._토.'.split('_'),
     longDateFormat: {
       LT: 'HH:mm',
       LTS: 'HH:mm:ss',
-      L: 'M 월 D 일',
-      LL: 'YYYY  M 월 DD 일',
+      L: 'M월 D일',
+      LL: 'YYYY년 M월 DD일',
       LLL: 'YYYY-MM-DD HH:mm',
       LLLL: 'YYYY-MM-DD HH:mm:ss',
       l: 'M/D',
@@ -41,19 +41,19 @@ moment.locale('ko', {
       }
     },
     relativeTime : {
-      future : '후 %s',
+      future : '%s 후',
       past : '%s 전',
       s : '몇초',
       m : '몇분',
-      mm : '%d 분',
+      mm : '%d분',
       h : '한시간',
-      hh : '%d 시간',
+      hh : '%d시간',
       d : '하루',
-      dd : '%d 일',
+      dd : '%d일',
       M : '한달',
-      MM : '%d 개월',
-      y : '일년',
-      yy : '%d 년'
+      MM : '%d개월',
+      y : '1년',
+      yy : '%d년'
     },
     week: {
       dow: 1,
